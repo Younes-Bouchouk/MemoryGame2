1e83c112e7d1de259fbfeb3dc99ac432605b0a86
Younès <Younes@MacBook-Pro-de-Youns.local>
2023-10-23 16:59:32 +0200

    Y - Debut PHP, arborescence des dossiers/fichiers, ajout du common.php

---
+++ partials/footer.php
@@ -0,0 +1,34 @@
+<footer>
+
+        <div id="foot">
+    
+            <div id="footer-left">
+                <h1>Information</h1>
+                <p>Si vous voulez nous contacter, vous pouvez <br> nous joindres via les informations suivantes</p>
+                <p><span>Tel :</span> 07 82 87 68 99 </p>
+                <p><span>Email :</span> jeromme.morand@gmail.com </p>
+                <p><span>Locatisation :</span> Cergy </p>
+        
+                <div>
+                    <a class="a" href="https://www.facebook.com/?locale=fr_FR" target="_blank"><img src="../components/assets/facebook.png"></img></a>
+                    <a class="a" href=" https://twitter.com/?lang=fr" target="_blank"><img src="../components/assets/twitter.png"></img></a>
+                    <a class="a" href="https://www.google.fr/" target="_blank"><img src="../components/assets/google.png"></img></a>
+                    <a class="a" href="https://www.pinterest.fr/" target="_blank"><img src="../components/assets/pinterest.png"></img></a>
+                    <a class="a" href="https://www.instagram.com/" target="_blank"><img src="../components/assets/instagram.png"></img></a>
+                </div>
+            </div>
+        
+            <div id="footer-right">
+                <h1>Power Of Memory</h1>
+                <ul>
+                    <a href="../Memory/levels.html">Jouer !</a>
+                    <a href="../Scores/scores.html">Les scores</a>
+                    <a href="../Contact/contact.html">Nous contacter</a>
+                </ul>
+            </div>
+        
+            <p id="cr">Copyright @ 2023 Tous droits réservés</p>
+    
+        </div>
+    
+    </footer>
---
+++ partials/header.php
@@ -0,0 +1,16 @@
+<header>
+        <h1 id="title">Memory Game</h1>
+        <nav>
+            <a href= "index.php">ACCUEIL</a>
+            <a href= "levels.php">JEU</a>
+            <a href= "scores.php">SCORES</a>
+            <a href= "contact.php">NOUS CONTACTER</a>
+            <a href= "login.php">CONNEXION</a>
+            <a href= "myAccount.php">MON COMPTE</a>
+        </nav>
+        <a id="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+</header>
